0d9d0db3fd5eaaa290f388cad08f678b3f9f5b18
ruben Toro <ruben.toro1@gmail.com>
2023-08-29 22:22:06 -0500

    Se hace ajuste de stilos y renombramos variables

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
+/** FORMULAS 
+1 monto prestamo = valor total - cuota inicial
+2 total interese =>(monto de prestamo) * tasa /100
+ Cuota =  (monto prestamo + total intereses) / (plazos años * 12 )
+ * 
+ * 
+ */
+ 
+const forma = document.getElementById['forma'];
+let totalIni = forma['cuotaIni'];
+let tasaInteres = forma['tasaInte'];
+let totalCasa = forma['tasa']
+
+
+
 function calcular(){
-    let cuota = 
+    
+    if()
 
 }
